175a9794c7b1fa826cb24b5ddf61a0cd80a77354
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2023-05-18 12:34:13 -0400

    cleaned up consolidated signals

--- data/data_consolidater.py
+++ data/data_consolidater.py
@@ -3,13 +3,15 @@ import pandas as pd
 # Read the data from the CSV files
 events_df = pd.read_csv('./donyc_data/total_donyc_events.csv')
 holidays_df = pd.read_csv('./nyc_holidays_data/holidays_data.csv')
+weather_df = pd.read_csv('./weather_data/weather_data.csv')
 
 # Convert date formats
 events_df['date'] = pd.to_datetime(events_df['date'])
 holidays_df['date'] = pd.to_datetime(holidays_df['date'], format='%m/%d/%Y')
+weather_df['date'] = pd.to_datetime(weather_df['date'])
 
 # Merge the dataframes based on the 'date' column
-consolidated_df = events_df.merge(holidays_df, on='date')
+consolidated_df = events_df.merge(holidays_df, on='date').merge(weather_df, on='date')
 
 # Save the consolidated dataframe as CSV
 consolidated_df.to_csv('consolidated_signals.csv', index=False)
